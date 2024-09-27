aa85fe6fd4932533fb78fecae3011db83f3de551
bhuvnesh996 <bhanushaily3@gmail.com>
2024-09-27 16:25:02 +0530

    fix

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 const production = 'https://searchmystudy.com/';
 const development = 'http://localhost:5000/'
-const baseQuery = fetchBaseQuery({ baseUrl: development });
+const baseQuery = fetchBaseQuery({ baseUrl: production });
 
 export const apiSlice = createApi({
   baseQuery,
